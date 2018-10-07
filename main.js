b892fa63db922dc1fe04db59174c48ac22c0d147
sophie <sophie.engel1202@gmail.com>
2018-10-07 11:05:26 +0200

    sticky menu

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
+"use strict";
+// svg animation //
 import TweenMax from "gsap/TweenMax";
 
-console.log("jesus");
+console.log("wtf?");
 
 TweenMax.staggerFrom(
   " #firkanter_1_ path",
   1,
   {
+    ease: Power4.easeOut,
     y: -1500
   },
-  0.05
+  0.03
 );
+
+// svg animation slut //
+
+// sticky menu //
+const nav = document.querySelector("#navigation");
+const navTop = nav.offsetTop;
+
+window.addEventListener("scroll", stickyNavigation);
+
+function stickyNavigation() {
+  if (window.scrollY >= navTop) {
+    nav.classList.add("fixed-nav");
+
+    console.log("sticky!!");
+  } else {
+    nav.classList.remove("fixed-nav");
+  }
+}
